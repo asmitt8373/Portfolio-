3ca98e3c0059d865c830c3b8bc358d1d16779a65
asmitt8373 <austinsmittle8373@gmail.com>
2025-01-08 11:31:48 -0800

    making it responsive

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,18 @@ export function Footer() {
     <div className="flex flex-rows  justify-center items-center mt-[100px] mb-[100px] md:mt-[125px] md:mb-[100px]">
       <div className="mr-[40px]">
         <a href="https://github.com/asmitt8373">
-          <img src="/img/github.png" className="h-[75px] w-[75px]" />
+          <img
+            src="/img/github.png"
+            className=" h-[50px] w-[50px] md:h-[75px] md:w-[75px]"
+          />
         </a>
       </div>
       <div>
         <a href="mailto:austinsmittle8373@gmail.com">
-          <img src="/img/mail.png" className="h-[75px] w-[75px]" />
+          <img
+            src="/img/mail.png"
+            className="h-[50px] w-[50px] md:h-[75px] md:w-[75px]"
+          />
         </a>
       </div>
     </div>
